7c001321f2279084b110781fe08685b1d78be44d
ChaituVR <yourchaitu@gmail.com>
2023-07-27 13:09:16 +0530

    Add schema and seed to docker

---
+++ src/helpers/seed.sql
@@ -0,0 +1,18 @@
+insert into
+  `keys` (
+    `active`,
+    `created`,
+    `key`,
+    `name`,
+    `owner`,
+    `updated`
+  )
+values
+  (
+    DEFAULT,
+    DEFAULT,
+    '123456789',
+    'test API key',
+    'test',
+    DEFAULT
+  );
