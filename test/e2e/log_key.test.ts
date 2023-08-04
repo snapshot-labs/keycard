e72ba9c1560dd487c6e2b2bf0b66ad8ae754fa58
ChaituVR <yourchaitu@gmail.com>
2023-08-04 16:09:13 +0530

    remove 429 test

--- test/e2e/log_key.test.ts
+++ test/e2e/log_key.test.ts
@@ -7,10 +7,6 @@ describe('logKey', () => {
     it.todo('returns a 401 error');
   });
 
-  describe('when the key has exceeded the monthly quota', () => {
-    it.todo('returns a 429 error');
-  });
-
   describe('when the key is active', () => {
     it.todo('increment the key total usage');
   });
