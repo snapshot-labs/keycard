d645a4981ded2665a5b957bd623cc43551656b28
Wan <495709+wa0x6e@users.noreply.github.com>
2023-08-08 11:28:58 +0400

    Update src/helpers/metrics.ts
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>

--- src/helpers/metrics.ts
+++ src/helpers/metrics.ts
@@ -6,21 +6,28 @@ export default function initMetrics(app: Express) {
   init(app, { whitelistedPath: [/^\/$/] });
 }
 
+async function collectSubscriberCounts() {
+  const subscriberCounts = await Promise.all([
+    db.queryAsync('SELECT count(*) as count FROM `keys` WHERE `key` IS NOT NULL'),
+    db.queryAsync('SELECT count(*) as count FROM `keys` WHERE `key` IS NULL')
+  ]);
+
+  return [
+    { status: 'active', count: subscriberCounts[0][0].count },
+    { status: 'pending', count: subscriberCounts[1][0].count }
+  ];
+}
+
 new client.Gauge({
-  name: 'subscribers_per_status_count',
-  help: 'Number of subscribers per status',
+  name: 'snapshot_subscriber_counts',
+  help: 'Number of Snapshot subscribers by status',
   labelNames: ['status'],
   async collect() {
-    [
-      ['active', '`key` IS NOT NULL'],
-      ['pending', '`key` IS NULL']
-    ].forEach(async function callback(this: any, data: any) {
-      this.set(
-        { status: data[0] },
-        (await db.queryAsync(`SELECT count(*) as count FROM \`keys\` WHERE ${data[1]}`))[0]
-          .count as any
-      );
-    }, this);
+    const subscriberCounts = await collectSubscriberCounts();
+
+    subscriberCounts.forEach(({ status, count }) => {
+      this.set({ status }, count);
+    });
   }
 });
 
